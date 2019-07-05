7639971729d67ee007dbe2a756721e962762b494
ZhangKaitlyn <1873070425@qq.com>
2019-07-05 22:14:05 +0800

    Footer Comp

--- app/web/components/Footer.jsx
+++ app/web/components/Footer.jsx
@@ -1,9 +1,25 @@
 import React, { Component } from 'react'
+import footerStyle from 'asset/css/module/footer.css'
+import ZcoolImg from 'asset/images/zcool.png'
+import QQImg from 'asset/images/QQ.png'
+import WeiboImg from 'asset/images/weibo.png'
+import WechatImg from 'asset/images/wechat.png'
+import EmailImg from 'asset/images/email.png'
 
 export default class Footer extends Component {
     render() {
         return (
-            < div className="footer-comp" >Footer</div >
+            < div className={footerStyle.footerComp}>
+                <div className={footerStyle.contactRow}>
+                    <img src={ZcoolImg} alt="zcool" />
+                    <img src={QQImg} alt="QQ" />
+                    <img src={WeiboImg} alt="weibo" />
+                    <img src={WechatImg} alt="wechat" />
+                    <img src={EmailImg} alt="email" />
+                </div>
+                <div className={footerStyle.recordNoRow}>鄂ICP备18001822号-1</div>
+                <div>&copy; 2019 Chiyu. 特别感谢 @ZhangKaitlyn</div>
+            </div >
         )
     }
 }
