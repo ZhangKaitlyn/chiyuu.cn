e869168dad14e0f9b69ea2780fb4bd82f9888cc4
ZhangKaitlyn <1873070425@qq.com>
2019-07-08 22:22:18 +0800

    works list

--- app/web/page/works/works.jsx
+++ app/web/page/works/works.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import WorksList from '@/components/WorksList'
 
 export default class Works extends Component {
     render() {
         return (
             <div className="works-page">
                 <Header></Header>
-                WorksPage
+                <WorksList></WorksList>
                 <Footer></Footer>
         </div>
         )
