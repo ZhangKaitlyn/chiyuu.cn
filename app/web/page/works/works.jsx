6b75acbe26b036d54a72c31ec7e31ffec272d46b
ZhangKaitlyn <1873070425@qq.com>
2019-07-14 19:09:43 +0800

    works list page fix

--- app/web/page/works/works.jsx
+++ app/web/page/works/works.jsx
@@ -2,15 +2,18 @@ import React, { Component } from 'react'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import WorksList from '@/components/WorksList'
+import worksPageStyle from "@/asset/css/module/worksPage.css"
 
 export default class Works extends Component {
     render() {
         return (
-            <div className="works-page">
+            <div>
                 <Header></Header>
+                <h1 className={worksPageStyle.title}>设计是一场永无止境的修行</h1>
+                <p className={worksPageStyle.info}>蔡小民 - UI设计&插画</p>
                 <WorksList></WorksList>
                 <Footer></Footer>
-        </div>
+            </div>
         )
     }
 }
