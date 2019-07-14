79f6e2a6f62aa477b276a5aca248d111c9433eb0
ZhangKaitlyn <1873070425@qq.com>
2019-07-14 19:58:45 +0800

    about page

--- app/web/page/about/about.jsx
+++ app/web/page/about/about.jsx
@@ -1,67 +1,73 @@
 import React, { Component } from 'react'
-import './about.css'
+import { Row, Col } from 'antd';
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+import aboutPageStyle from "@/asset/css/module/aboutPage.css"
 import aboutPic from 'asset/images/about_pic.png'
 
 export default class About extends Component {
   render() {
     return (
-      <div className="about-comp">
-        <div className="about-container">
-          <div className="about-title">
-            关于 <span className="about-title-sub">About</span>
-          </div>
-          <div className="about-content">
-            <div className="about-author">蔡小民</div>
-            <div className="about-type-item">
-              <div className="about-type-title">履历</div>
-              <div className="about-info-item">
-                <div className="about-info-item-title">
-                  深圳市大富网络技术有限公司{' '}
-                  <span className="about-info-item-job">UI设计师</span>
-                  <span className="about-info-item-time">2018.3 - 至今</span>
+      <div className={aboutPageStyle.aboutPage}>
+        <Header></Header>
+        <Row className={aboutPageStyle.content}>
+          <Col span={12} className={aboutPageStyle.imgZone}><img src={aboutPic} alt="" /></Col>
+          <Col span={12} className={aboutPageStyle.infoZone}>
+            <h1 className={aboutPageStyle.pageTitle}>蔡小民 Chiyu</h1>
+            <div className={aboutPageStyle.item}>
+              <div className={aboutPageStyle.itemTitle}>履历</div>
+              <div className={aboutPageStyle.infoItem}>
+                <div className={aboutPageStyle.infoTitle}>
+                  <div className={aboutPageStyle.flexLeft}>
+                    深圳市大富网络技术有限公司{' '}UI设计师
+                  </div>
+                  <span className={aboutPageStyle.time}>2018.3 - 至今</span>
                 </div>
-                <div className="about-info-item-intro">
+                <div className={aboutPageStyle.intro}>
                   负责公司网站视觉和体验优化和迭代，参与并主导知识引擎app的设计
                 </div>
               </div>
-              <div className="about-info-item">
-                <div className="about-info-item-title">
-                  深圳市一米互动科技有限公司
-                  <span className="about-info-item-job">UI设计师</span>
-                  <span className="about-info-item-time">2017.6-2018.3</span>
+              <div className={aboutPageStyle.infoItem}>
+                <div className={aboutPageStyle.infoTitle}>
+                  <div className={aboutPageStyle.flexLeft}>
+                    深圳市一米互动科技有限公司{' '}UI设计师
+                  </div>
+                  <span className={aboutPageStyle.time}>2017.6-2018.3</span>
                 </div>
-                <div className="about-info-item-intro">
+                <div className={aboutPageStyle.intro}>
                   负责公司所有的设计工作，app、小程序、网站、品牌等
                 </div>
               </div>
             </div>
-            <div className="about-type-item">
-              <div className="about-type-title">教育</div>
-              <div className="about-info-item">
-                <div className="about-info-item-title">
-                  武汉工程大学
-                  <span className="about-info-item-time">2013.9 - 2017.6</span>
+            <div className={aboutPageStyle.item}>
+              <div className={aboutPageStyle.itemTitle}>教育</div>
+              <div className={aboutPageStyle.infoItem}>
+                <div className={aboutPageStyle.infoTitle}>
+                  <div className={aboutPageStyle.flexLeft}>
+                    武汉工程大学
+                  </div>
+                  <span className={aboutPageStyle.time}>2013.9 - 2017.6</span>
                 </div>
-                <div className="about-info-item-intro">
+                <div className={aboutPageStyle.intro}>
                   2013年考入武汉工程大学艺术设计学院视觉传达专业
                 </div>
               </div>
-              <div className="about-info-item">
-                <div className="about-info-item-title">
-                  三峡艺术高中
-                  <span className="about-info-item-time">2010.9 - 2013.6</span>
+              <div className={aboutPageStyle.infoItem}>
+                <div className={aboutPageStyle.infoTitle}>
+                  <div className={aboutPageStyle.flexLeft}>
+                    三峡艺术高中
+                  </div>
+                  <span className={aboutPageStyle.time}>2010.9 - 2013.6</span>
                 </div>
-                <div className="about-info-item-intro">
+                <div className={aboutPageStyle.intro}>
                   3年艺术生涯是我们为梦想拼搏奋斗、难以忘怀的时光
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div className="about-bg">
-          <img src={aboutPic} alt="" />
-        </div>
-      </div>
+          </Col>
+        </Row>
+        <Footer></Footer>
+      </div >
     )
   }
 }
