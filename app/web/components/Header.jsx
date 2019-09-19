1d4355e7dbc376a4616f88c3529153c9997c3654
ZhangKaitlyn <1873070425@qq.com>
2019-09-19 08:27:17 +0800

    fix phone

--- app/web/components/Header.jsx
+++ app/web/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Row, Col } from 'antd';
+import { Row, Col, Drawer, Button } from 'antd';
 import LogoImg from 'asset/images/logo.png'
 import PersonImg from 'asset/images/person.png'
 import headerStyles from 'asset/css/module/header.css'
@@ -8,24 +8,52 @@ import { init } from '../lib/eyeCanvas'
 export default class Header extends Component {
   constructor(props) {
     super(props)
+    this.state = { visible: false }
   }
   componentDidMount() {
     init()
   }
+  showDrawer() {
+    this.setState({
+      visible: true,
+    })
+  }
+  onClose() {
+    this.setState({
+      visible: false,
+    })
+  }
+
   render() {
     return (
-      <Row className={headerStyles.headerRow}>
-        <Col span={8}>
-          <img src={LogoImg} alt="Chiyuu" />
-        </Col>
-        <Col span={8} className={headerStyles.personImgCol}>
-          <canvas className={headerStyles.canvas}></canvas>
-        </Col>
-        <Col span={8} className={headerStyles.linksCol}>
-          <a href='/works'>作品</a>
-          <a href='/about'>关于</a>
-        </Col>
-      </Row>
+      <div>
+        <Row className={headerStyles.headerRow}>
+          <Col span={8}>
+            <img src={LogoImg} alt="Chiyuu" />
+          </Col>
+          <Col span={8} className={headerStyles.personImgCol}>
+            <canvas className={headerStyles.canvas}></canvas>
+          </Col>
+          <Col sm={0} md={8} className={headerStyles.linksCol}>
+            <a href='/works'>作品</a>
+            <a href='/about'>关于</a>
+          </Col>
+          <Col sm={8} md={0}>
+            <Button icon="menu" onClick={this.showDrawer}></Button>
+          </Col>
+        </Row>
+        <Drawer
+          title="Basic Drawer"
+          placement="top"
+          closable={true}
+          onClose={this.onClose}
+          visible={this.state.visible}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Drawer>
+      </div>
     )
   }
 }
