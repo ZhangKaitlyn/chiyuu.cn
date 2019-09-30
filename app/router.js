830cec60aa5761b64dad763916d835fb4b76d8fa
ZhangKaitlyn <1873070425@qq.com>
2019-09-30 19:02:19 +0800

    backend

--- app/router.js
+++ app/router.js
@@ -1,12 +1,13 @@
-
 module.exports = app => {
   // app.get('/', app.controller.home.home.index);
-  app.get('/', app.controller.works.index);
-  app.get('/works', app.controller.works.index);
-  app.get('/detail', app.controller.works.detail);
-  app.get('/about', app.controller.about.index);
-  app.post('/works', app.controller.backend.works.getWorksList);
-  app.get('/back/login', app.controller.back.login);
-  app.get('/back/list', app.controller.back.list);
-  app.get('/back/upload', app.controller.back.upload);
-};
+  app.get('/', app.controller.works.index)
+  app.get('/works', app.controller.works.index)
+  app.get('/detail', app.controller.works.detail)
+  app.get('/about', app.controller.about.index)
+
+  app.get('/api/works', app.controller.backend.works.getWorksList)
+
+  app.get('/back/login', app.controller.back.login)
+  app.get('/back/list', app.controller.back.list)
+  app.get('/back/upload', app.controller.back.upload)
+}
--- app/web/page/back/List.jsx
+++ app/web/page/back/List.jsx
@@ -1,8 +1,24 @@
 import React, { Component } from 'react'
 import Header from '@/components/backend/Header'
 import Footer from '@/components/backend/Footer'
+import axios from 'axios'
 
 export default class List extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      works: []
+    }
+  }
+  async componentDidMount() {
+    try {
+      this.setState({
+        works: await axios.get('/api/works')
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
   render() {
     return (
       <div>
