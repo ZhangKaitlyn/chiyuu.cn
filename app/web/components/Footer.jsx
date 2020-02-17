ddb2afbcf0a68eb1c376c4bb5c52540827773466
ZhangKaitlyn <1873070425@qq.com>
2020-02-17 21:47:26 +0800

    fix footer tooltip

--- app/web/components/Footer.jsx
+++ app/web/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Tooltip } from 'antd'
 import footerStyle from 'asset/css/module/footer.css'
 import ZcoolImg from 'asset/images/zcool.png'
 import QQImg from 'asset/images/QQ.png'
@@ -11,11 +12,21 @@ export default class Footer extends Component {
         return (
             < div className={footerStyle.footerComp}>
                 <div className={footerStyle.contactRow}>
-                    <img src={ZcoolImg} alt="zcool" />
-                    <img src={QQImg} alt="QQ" />
-                    <img src={WeiboImg} alt="weibo" />
-                    <img src={WechatImg} alt="wechat" />
-                    <img src={EmailImg} alt="email" />
+                    <Tooltip title="迟語" overlayClassName={footerStyle.tooltipContainer}>
+                        <img src={ZcoolImg} alt="zcool" />
+                    </Tooltip>
+                    <Tooltip title="chiyuu" overlayClassName={footerStyle.tooltipContainer}>
+                        <img src={QQImg} alt="QQ" />
+                    </Tooltip>
+                    <Tooltip title="@蔡小民明铭" overlayClassName={footerStyle.tooltipContainer}>
+                        <img src={WeiboImg} alt="weibo" />
+                    </Tooltip>
+                    <Tooltip title="mmm1220i" overlayClassName={footerStyle.tooltipContainer}>
+                        <img src={WechatImg} alt="wechat" />
+                    </Tooltip>
+                    <Tooltip title="374631450@qq.com" overlayClassName={footerStyle.tooltipContainer}>
+                        <img src={EmailImg} alt="email" />
+                    </Tooltip>
                 </div>
                 <div className={footerStyle.recordNoRow}>鄂ICP备18001822号-1</div>
                 <div>&copy; 2019 Chiyu. 特别感谢 @ZhangKaitlyn</div>
