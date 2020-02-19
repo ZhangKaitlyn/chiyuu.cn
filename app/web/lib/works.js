15e1acf921727348e63de9930d23e68bc4565df5
ZhangKaitlyn <1873070425@qq.com>
2020-02-19 15:06:05 +0800

    配置作品列表

--- app/web/lib/works.js
+++ app/web/lib/works.js
@@ -1,16 +1,16 @@
 export const works = [
-  {
-    id: 1,
-    type: 'multiple',
-    title: '作品集&简历[快速通道]',
-    info: '其他-作品集',
-    coverImg: 'http://q5u4vw4fu.bkt.clouddn.com/1.%E4%BD%9C%E5%93%81%E9%9B%86&%E7%AE%80%E5%8E%86.png',
-    images: [
-      'https://images.unsplash.com/photo-1564074621995-7c1f9a8ea9a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80',
-      'https://images.unsplash.com/photo-1564084571423-7894bb30fe80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-      'https://images.unsplash.com/photo-1556911220-3bdc5f08618e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
-    ]
-  },
+  // {
+  //   id: 1,
+  //   type: 'multiple',
+  //   title: '作品集&简历[快速通道]',
+  //   info: '其他-作品集',
+  //   coverImg: 'http://q5u4vw4fu.bkt.clouddn.com/1.%E4%BD%9C%E5%93%81%E9%9B%86&%E7%AE%80%E5%8E%86.png',
+  //   images: [
+  //     'https://images.unsplash.com/photo-1564074621995-7c1f9a8ea9a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80',
+  //     'https://images.unsplash.com/photo-1564084571423-7894bb30fe80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  //     'https://images.unsplash.com/photo-1556911220-3bdc5f08618e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+  //   ]
+  // },
   {
     id: 2,
     type: 'multiple',
@@ -19,9 +19,35 @@ export const works = [
     info: 'app - redesign',
     coverImg: 'http://q5u4vw4fu.bkt.clouddn.com/2.%E4%B8%80%E7%BD%90app%28%E6%96%B0%29.png',
     images: [
-      'https://images.unsplash.com/photo-1564074621995-7c1f9a8ea9a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80',
-      'https://images.unsplash.com/photo-1564084571423-7894bb30fe80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-      'https://images.unsplash.com/photo-1556911220-3bdc5f08618e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+      'http://q5u4vw4fu.bkt.clouddn.com/app-yiguan-1.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/app-yiguan-2.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/app-yiguan-3.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/app-yiguan-4.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/app-yiguan-5.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/app-yiguan-6.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/app-yiguan-7.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/app-yiguan-8.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/app-yiguan-9.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/app-yiguan-9-1.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/app-yiguan-9-2.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/app-yiguan-10.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/app-yiguan-10-1.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/app-yiguan-10-2.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/app-yiguan-10-3.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/app-yiguan-11.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/app-yiguan-12.png',
+    ]
+  },
+  {
+    id: '2-1',
+    type: 'multiple',
+    title: 'Paracraft新手试玩项目',
+    info: 'UI - 客户端',
+    coverImg: 'http://q5u4vw4fu.bkt.clouddn.com/paracaraf%E5%B0%81%E9%9D%A2.png',
+    images: [
+      'http://q5u4vw4fu.bkt.clouddn.com/web-kp-17.jpg',
+      'http://q5u4vw4fu.bkt.clouddn.com/web-kp-18.jpg',
+      'http://q5u4vw4fu.bkt.clouddn.com/web-kp-19.png'
     ]
   },
   {
@@ -46,11 +72,7 @@ export const works = [
       'http://q5u4vw4fu.bkt.clouddn.com/web-kp-13.gif',
       'http://q5u4vw4fu.bkt.clouddn.com/web-kp-14.jpg',
       'http://q5u4vw4fu.bkt.clouddn.com/web-kp-15.jpg',
-      'http://q5u4vw4fu.bkt.clouddn.com/web-kp-16.gif',
-      'http://q5u4vw4fu.bkt.clouddn.com/web-kp-17.jpg',
-      'http://q5u4vw4fu.bkt.clouddn.com/web-kp-18.jpg',
-      'http://q5u4vw4fu.bkt.clouddn.com/web-kp-19.jpg',
-      'http://q5u4vw4fu.bkt.clouddn.com/web-kp-20.gif'
+      'http://q5u4vw4fu.bkt.clouddn.com/web-kp-16.gif'
     ]
   },
   {
@@ -60,9 +82,18 @@ export const works = [
     info: 'Web - 概念设计',
     coverImg: 'http://q5u4vw4fu.bkt.clouddn.com/4.anglee.png',
     images: [
-      'https://images.unsplash.com/photo-1564074621995-7c1f9a8ea9a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80',
-      'https://images.unsplash.com/photo-1564084571423-7894bb30fe80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-      'https://images.unsplash.com/photo-1556911220-3bdc5f08618e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+      'http://q5u4vw4fu.bkt.clouddn.com/web-anglee-1.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/web-anglee-2.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/web-anglee-3.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/web-anglee-4.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/web-anglee-5.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/web-anglee-6.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/web-anglee-7.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/web-anglee-8.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/web-anglee-9.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/web-anglee-10.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/web-anglee-11.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/web-anglee-12.png',
     ]
   },
   {
@@ -72,9 +103,9 @@ export const works = [
     info: 'UI - 客户端',
     coverImg: 'http://q5u4vw4fu.bkt.clouddn.com/5.%E7%9F%A5%E8%AF%86%E5%BC%95%E6%93%8E.png',
     images: [
-      'https://images.unsplash.com/photo-1564074621995-7c1f9a8ea9a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80',
-      'https://images.unsplash.com/photo-1564084571423-7894bb30fe80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-      'https://images.unsplash.com/photo-1556911220-3bdc5f08618e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+      'http://q5u4vw4fu.bkt.clouddn.com/client-keepnote-1.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/client-keepnote-2.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/client-keepnote-3.png',
     ]
   },
   {
@@ -84,9 +115,13 @@ export const works = [
     info: '插画 - 简笔画',
     coverImg: 'http://q5u4vw4fu.bkt.clouddn.com/6.%E7%AE%80%E7%AC%94%E7%94%BB%E6%97%A5%E8%AE%B0.png',
     images: [
-      'https://images.unsplash.com/photo-1564074621995-7c1f9a8ea9a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80',
-      'https://images.unsplash.com/photo-1564084571423-7894bb30fe80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-      'https://images.unsplash.com/photo-1556911220-3bdc5f08618e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+      'http://q5u4vw4fu.bkt.clouddn.com/sticky-7.gif',
+      'http://q5u4vw4fu.bkt.clouddn.com/sticky-6.gif',
+      'http://q5u4vw4fu.bkt.clouddn.com/sticky-5.gif',
+      'http://q5u4vw4fu.bkt.clouddn.com/sticky-4.gif',
+      'http://q5u4vw4fu.bkt.clouddn.com/sticky-3.gif',
+      'http://q5u4vw4fu.bkt.clouddn.com/sticky-2.gif',
+      'http://q5u4vw4fu.bkt.clouddn.com/sticky-1.gif'
     ]
   },
   {
@@ -96,23 +131,29 @@ export const works = [
     info: '插画 - 手绘',
     coverImg: 'http://q5u4vw4fu.bkt.clouddn.com/7.%E6%89%8B%E7%BB%98%E6%8F%92%E7%94%BB.png',
     images: [
-      'https://images.unsplash.com/photo-1564074621995-7c1f9a8ea9a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80',
-      'https://images.unsplash.com/photo-1564084571423-7894bb30fe80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-      'https://images.unsplash.com/photo-1556911220-3bdc5f08618e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+      'http://q5u4vw4fu.bkt.clouddn.com/Hp-1.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/Hp-2.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/Hp-3.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/Hp-4.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/Hp-5.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/Hp-6.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/Hp-7.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/Hp-8.png',
+      'http://q5u4vw4fu.bkt.clouddn.com/Hp-9.png'
     ]
   },
-  {
-    id: 8,
-    type: 'multiple',
-    title: '办会家品牌标识',
-    info: '平面 - 品牌',
-    coverImg: 'http://q5u4vw4fu.bkt.clouddn.com/8.%E5%8A%9E%E4%BC%9A%E5%AE%B6%E6%A0%87%E8%AF%86.png',
-    images: [
-      'https://images.unsplash.com/photo-1564074621995-7c1f9a8ea9a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80',
-      'https://images.unsplash.com/photo-1564084571423-7894bb30fe80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-      'https://images.unsplash.com/photo-1556911220-3bdc5f08618e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
-    ]
-  },
+  // {
+  //   id: 8,
+  //   type: 'multiple',
+  //   title: '办会家品牌标识',
+  //   info: '平面 - 品牌',
+  //   coverImg: 'http://q5u4vw4fu.bkt.clouddn.com/8.%E5%8A%9E%E4%BC%9A%E5%AE%B6%E6%A0%87%E8%AF%86.png',
+  //   images: [
+  //     'https://images.unsplash.com/photo-1564074621995-7c1f9a8ea9a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80',
+  //     'https://images.unsplash.com/photo-1564084571423-7894bb30fe80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  //     'https://images.unsplash.com/photo-1556911220-3bdc5f08618e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+  //   ]
+  // },
   {
     id: 9,
     type: 'multiple',
@@ -120,34 +161,34 @@ export const works = [
     info: 'UI - 移动端app',
     coverImg: 'http://q5u4vw4fu.bkt.clouddn.com/9.%E4%BC%9A%E7%AD%BE%E5%88%B0app.png',
     images: [
-      'https://images.unsplash.com/photo-1564074621995-7c1f9a8ea9a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80',
-      'https://images.unsplash.com/photo-1564084571423-7894bb30fe80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-      'https://images.unsplash.com/photo-1556911220-3bdc5f08618e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+      'http://q5u4vw4fu.bkt.clouddn.com/app-hqd-1.jpg',
+      'http://q5u4vw4fu.bkt.clouddn.com/app-hqd-2.jpg',
+      'http://q5u4vw4fu.bkt.clouddn.com/app-hqd-3.jpg',
     ]
   },
-  {
-    id: 10,
-    type: 'multiple',
-    title: 'Like in space',
-    info: '平面 - 耳机海报',
-    coverImg: 'http://q5u4vw4fu.bkt.clouddn.com/10.like%20in%20space.png',
-    images: [
-      'https://images.unsplash.com/photo-1564074621995-7c1f9a8ea9a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80',
-      'https://images.unsplash.com/photo-1564084571423-7894bb30fe80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-      'https://images.unsplash.com/photo-1556911220-3bdc5f08618e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
-    ]
-  },
-  {
-    id: 11,
-    type: 'multiple',
-    title: '汉阳区今日美术馆设计',
-    info: '空间 - 建筑设计',
-    coverImg: 'http://q5u4vw4fu.bkt.clouddn.com/11.%E6%B1%89%E9%98%B3%E5%8C%BA%E4%BB%8A%E6%97%A5%E7%BE%8E%E6%9C%AF%E9%A6%86%E8%AE%BE%E8%AE%A1-%E6%AF%95%E4%B8%9A%E8%AE%BE%E8%AE%A1.png',
-    images: [
-      'https://images.unsplash.com/photo-1564074621995-7c1f9a8ea9a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80',
-      'https://images.unsplash.com/photo-1564084571423-7894bb30fe80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-      'https://images.unsplash.com/photo-1556911220-3bdc5f08618e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
-    ]
-  }
+  // {
+  //   id: 10,
+  //   type: 'multiple',
+  //   title: 'Like in space',
+  //   info: '平面 - 耳机海报',
+  //   coverImg: 'http://q5u4vw4fu.bkt.clouddn.com/10.like%20in%20space.png',
+  //   images: [
+  //     'https://images.unsplash.com/photo-1564074621995-7c1f9a8ea9a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80',
+  //     'https://images.unsplash.com/photo-1564084571423-7894bb30fe80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  //     'https://images.unsplash.com/photo-1556911220-3bdc5f08618e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+  //   ]
+  // },
+  // {
+  //   id: 11,
+  //   type: 'multiple',
+  //   title: '汉阳区今日美术馆设计',
+  //   info: '空间 - 建筑设计',
+  //   coverImg: 'http://q5u4vw4fu.bkt.clouddn.com/11.%E6%B1%89%E9%98%B3%E5%8C%BA%E4%BB%8A%E6%97%A5%E7%BE%8E%E6%9C%AF%E9%A6%86%E8%AE%BE%E8%AE%A1-%E6%AF%95%E4%B8%9A%E8%AE%BE%E8%AE%A1.png',
+  //   images: [
+  //     'https://images.unsplash.com/photo-1564074621995-7c1f9a8ea9a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80',
+  //     'https://images.unsplash.com/photo-1564084571423-7894bb30fe80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  //     'https://images.unsplash.com/photo-1556911220-3bdc5f08618e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+  //   ]
+  // }
 ]
 export default works
