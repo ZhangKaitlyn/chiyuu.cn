8841565cf7b13ae33fd7a08cb206c43354aa14b9
ZhangKaitlyn <1873070425@qq.com>
2020-02-24 14:59:47 +0800

    fix footer icon

--- app/web/components/Footer.jsx
+++ app/web/components/Footer.jsx
@@ -1,32 +1,21 @@
 import React, { Component } from 'react'
 import { Tooltip } from 'antd'
 import footerStyle from 'asset/css/module/footer.css'
-import ZcoolImg from 'asset/images/zcool.png'
-import QQImg from 'asset/images/QQ.png'
-import WeiboImg from 'asset/images/weibo.png'
-import WechatImg from 'asset/images/wechat.png'
-import EmailImg from 'asset/images/email.png'
 
 export default class Footer extends Component {
     render() {
+        const iconsListTitles = ['迟語', 'chiyuu', '蔡小民明铭', 'mmm1220i', '374631450@qq.com']
+        const iconsListDom = iconsListTitles.map((title, index) => {
+            const positionSpace = -32 * index + 'px'
+            const marginSpace = index > 0 ? '32px' : 0
+            return <Tooltip title={title} overlayClassName={footerStyle.tooltipContainer}>
+                <div className={footerStyle.icon} style={{ 'background-position': positionSpace, 'margin-left': marginSpace }}></div>
+            </Tooltip >
+        })
         return (
-            < div className={footerStyle.footerComp}>
+            <div className={footerStyle.footerComp} >
                 <div className={footerStyle.contactRow}>
-                    <Tooltip title="迟語" overlayClassName={footerStyle.tooltipContainer}>
-                        <img src={ZcoolImg} alt="zcool" />
-                    </Tooltip>
-                    <Tooltip title="chiyuu" overlayClassName={footerStyle.tooltipContainer}>
-                        <img src={QQImg} alt="QQ" />
-                    </Tooltip>
-                    <Tooltip title="@蔡小民明铭" overlayClassName={footerStyle.tooltipContainer}>
-                        <img src={WeiboImg} alt="weibo" />
-                    </Tooltip>
-                    <Tooltip title="mmm1220i" overlayClassName={footerStyle.tooltipContainer}>
-                        <img src={WechatImg} alt="wechat" />
-                    </Tooltip>
-                    <Tooltip title="374631450@qq.com" overlayClassName={footerStyle.tooltipContainer}>
-                        <img src={EmailImg} alt="email" />
-                    </Tooltip>
+                    {iconsListDom}
                 </div>
                 <div className={footerStyle.recordNoRow}>鄂ICP备18001822号-1</div>
                 <div>&copy; 2019 Chiyu. 特别感谢 @ZhangKaitlyn</div>
